ee6305261bc52bfac72005ffc6faec8354026faf
PalomaCarrasco <80793260+PalomaCarrasco@users.noreply.github.com>
2021-05-19 21:35:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function primeraRevista() {
-    document.getElementsByTagName("body")[0].style.background = "#D34145";
+    document.getElementsByTagName("body")[0].style.background = "#9D5401";
 }
 function segundaRevista() {
     document.getElementsByTagName("body")[0].style.background = "#5790A3";
 }
 function terceraRevista() {
-    document.getElementsByTagName("body")[0].style.background = "#6A5D9C";
+    document.getElementsByTagName("body")[0].style.background = "#9F0404";
 }
 function cuartaRevista() {
-    document.getElementsByTagName("body")[0].style.background = "#117C60";
+    document.getElementsByTagName("body")[0].style.background = "#000";
 }
 function nada() {
     document.getElementsByTagName("body")[0].style.background = "#222";
